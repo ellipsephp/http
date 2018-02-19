96a64b7ad42e137d28a41111e36e6302eed66b4f
pmall <pierre.mallinjoud@gmail.com>
2018-02-19 12:00:31 +0100

    remove default template

--- templates/default.php
+++
@@ -1,5 +0,0 @@
-<?php $this->layout('layout', ['title' => 'Welcome to ellipse!']) ?>
-<h1>Welcome to ellipse!</h1>
-<p>
-    This is the default request handler. This means no middleware produced a response before hitting it.
-</p>
